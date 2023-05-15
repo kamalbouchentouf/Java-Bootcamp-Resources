9e74dfd4abb8ee9f75af76abc5be7f77854ffe12
bouchentouf kamal <bouchentouf.kamal@gmail.com>
2023-05-15 10:46:33 +0200

    Module 1 - 1 - Challenge 1

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,12 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("K   K    A    M   M    A    L");
+         System.out.println("K  K    A A   MM MM   A A   L");
+         System.out.println("K K    A   A  M M M  A   A  L");
+         System.out.println("KK     AAAAA  M   M  AAAAA  L");
+         System.out.println("K K    A   A  M   M  A   A  L");
+         System.out.println("K  K   A   A  M   M  A   A  L");
+         System.out.println("K   K  A   A  M   M  A   A  LLLLL");
     }
 }
