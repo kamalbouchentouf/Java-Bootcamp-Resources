e294ec3932a9330e3666a92f529a020488f19faa
bouchentouf kamal <bouchentouf.kamal@gmail.com>
2023-05-16 11:05:31 +0200

    Module 1 - 3 - Challenge 3

--- Module 1 - Introduction to Java/3. Booleans and Conditionals/Challenge 3/Quiz.java
+++ Module 1 - Introduction to Java/3. Booleans and Conditionals/Challenge 3/Quiz.java
@@ -10,24 +10,60 @@ public class Quiz {
 
         System.out.println("1. Which country held the 2016 Summer Olympics?");
         System.out.println("\ta) China\n\tb) Ireland\n\tc) Brazil\n\td) Italy\n");
-        //store answer 1
+        String answerOne = scan.nextLine(); 
 
         System.out.println("\n2. Which planet is the hottest?");
         System.out.println("\ta) Venus\n\tb) Saturn\n\tc) Mercury\n\td) Mars\n");
-        //store answer 2
+        String answerTwo = scan.nextLine();
 
         System.out.println("\n3. What is the rarest blood type?");
         System.out.println("\ta) O\n\tb) A\n\tc) B\n\td) AB-Negative\n");
-        //store answer 3
+        String answerThree = scan.nextLine();
 
         System.out.println("\n4. Which one of these characters is friends with Harry Potter?");
         System.out.println("\ta) Ron Weasley\n\tb) Hermione Granger\n\tc) Draco Malfoy\n");
-        //store answer 4
+        String answerFour = scan.nextLine();
 
         int score = 0;
 
 
-        //Task 2: Check each answer - For each correct answer, add 5 points to the score. 
+        //Task 2: Check each answer - For each correct answer, add 5 points to the score.
+        
+        switch (answerOne) {
+            case "a": break;
+            case "b": break;
+            case "c": score +=5 ; break ;
+            case "d": break;
+            default:
+                break;
+        }
+
+        switch (answerTwo) {
+            case "a": score +=5 ; break ;
+            case "b": break;
+            case "c": break ;
+            case "d": break;
+            default:
+                break;
+        }
+
+        switch (answerThree) {
+            case "a": break;
+            case "b": break;
+            case "c": break ;
+            case "d": score +=5 ; break ;
+            default:
+                break;
+        }
+
+        switch (answerFour) {
+            case "a": score +=5 ; break ;
+            case "b": break;
+            case "c": break ;
+            case "d": break;
+            default:
+                break;
+        }
         
         System.out.println("Your final score is: " + score + "/20");
 
@@ -36,6 +72,13 @@ public class Quiz {
         // if the score is 15 or higher, print: "Wow, you know your stuff!";
         // if the score is between 5 and 15, print "Not bad!";
         // else, better luck next time. 
+        if (score >= 15) {
+            System.out.println("Wow, you know your stuff!");
+        } else if (score >= 5 && score < 15) {
+            System.out.println("Not bad!");
+        } else {
+            System.out.println("better luck next time");
+        }
 
 
         scan.close();
